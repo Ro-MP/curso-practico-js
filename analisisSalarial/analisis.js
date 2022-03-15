48ad37b5320dd21a8286ebafc423b3a041d6eac0
Rodrigo MP <r.mpicazo.97gmail.com>
2022-03-15 15:07:43 -0600

    Olvide guardar un archivo, ahora si es el ultimo

--- analisisSalarial/analisis.js
+++ analisisSalarial/analisis.js
@@ -34,30 +34,6 @@ const spliceCount = salariosSorted.length - spliceStar;
 const salariosTop10 = salariosSorted.splice(spliceStar, spliceCount);
 const medianaTop10 = calcularMediana(salariosTop10);
 
-// Calculador Moda
-
-function calcularModa(lista){
-    const listaCount = {};
-
-    lista.map (
-        function(elemento) {
-            if (listaCount[elemento]){
-                listaCount[elemento] ++;
-            } else {
-                listaCount[elemento] = 1;
-            }
-        } 
-    );
-
-    const listaArray = Object.entries(listaCount).sort(
-        function(a, b){
-            return a[1]-b[1];
-        }
-    );
-
-    return listaArray[listaArray.length - 1]
-}
-
 
 console.log({
     medianaGeneral,
